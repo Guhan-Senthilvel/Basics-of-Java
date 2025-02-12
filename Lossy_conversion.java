a2e97802df7731bbaeded55bb29a49a32c294846
Guhan-Senthilvel <121353997+Guhan-Senthilvel@users.noreply.github.com>
2025-02-12 14:04:53 +0530

    Add files via upload
    Lossy conversion from int to byte

---
+++ Lossy_conversion.java
@@ -0,0 +1,16 @@
+class Lossy_conversion
+{
+	public static void main(String s[])
+	{
+		byte a=120;
+		byte b=100;
+		byte add,mul;
+                add=(byte)(a+b)
+		mul=(byte)(a*b);
+		System.out.println(add);
+		System.out.println(a-b);
+		System.out.println(mul);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
