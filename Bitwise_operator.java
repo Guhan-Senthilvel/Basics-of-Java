489b08b467610f00852ee140f90541548e0e5ab4
Guhan-Senthilvel <121353997+Guhan-Senthilvel@users.noreply.github.com>
2025-02-12 14:10:57 +0530

    Add files via upload
    Performing Bitwise operations

---
+++ Bitwise_operator.java
@@ -0,0 +1,20 @@
+class Bitwise_operator
+{
+	public static void main(String[] args)
+	{
+	    int a=492;
+	    int b=333;
+	    int c=a&b;
+	    int d=a|b;
+	    int e=a^b;
+	    int f=~a;
+	    
+	    System.out.println("Bitwise AND : " + c);
+	    System.out.println("Bitwise OR : " + d);
+	    System.out.println("Bitwise EX-OR : " + e);
+	    System.out.println("Bitwise NOT: " + f);
+	    
+	    
+	
+	}
+}
