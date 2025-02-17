20d96fb2072749a40fb50351586b70084565d92f
Guhan-Senthilvel <121353997+Guhan-Senthilvel@users.noreply.github.com>
2025-02-17 10:25:27 +0530

    Add files via upload

---
+++ for_each.java
@@ -0,0 +1,33 @@
+class for_each
+{
+	public static void main(String[] args) 
+	{
+	    int arr[]={1,2,3,4,5};
+	    
+	    for(int i=0;i<arr.length;i++)
+	    {
+	        System.out.println(arr[i]);
+	    }
+	    System.out.println();
+	    
+	    for(int i:arr)
+	    {
+	        i++;
+	        System.out.println(i);
+	    }
+	    System.out.println();
+	    for(int i=0;i<arr.length;i++)
+	    {
+
+	        System.out.println(arr[i]);
+	    }
+	    System.out.println();
+	    for(int i=arr.length-1;i>=0;i--)
+	    {
+	        System.out.println(arr[i]);
+	        
+	    }
+	    
+	    
+	}
+}
